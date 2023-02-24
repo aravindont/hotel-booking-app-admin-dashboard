ec55a9b9f6aa2d87491f1100d45ba38cbb169b37
Aravind104 <ontagodiaravind@gmail.com>
2023-02-24 20:13:00 +0530

    feat(hooks and context):add custom hook called useFetch and AuthContext to validate user login

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { AuthContextProvider } from "./context/AuthContext";
 import { ThemeContextProvider } from "./context/ThemeContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ThemeContextProvider>
-      <App />
-    </ThemeContextProvider>
+    <AuthContextProvider>
+      <ThemeContextProvider>
+        <App />
+      </ThemeContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,17 +1,39 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
 import "./login.scss";
+import { api } from "../../api";
 function LoginPage() {
   const [credentials, setCredentials] = useState({
     username: null,
     password: null,
   });
-
+  const { loading, error, dispatch } = useContext(AuthContext);
+  const navigate = useNavigate();
   const handleChange = (e) => {
     setCredentials((prev) => ({ ...prev, [e.target.id]: e.target.value }));
   };
 
   const handleLogin = async (e) => {
     e.preventDefault();
+    dispatch({ type: "LOGIN_START" });
+    try {
+      const res = await axios.post(`${api}/api/v1/auth/login`, credentials);
+      if (res.data.isAdmin) {
+        dispatch({ type: "LOGIN_SUCCESS", payload: res.data.details });
+      } else {
+        dispatch({
+          type: "LOGIN_FAILURE",
+          payload: { message: "You are not allowed!" },
+        });
+      }
+    } catch (err) {
+      dispatch({
+        type: "LOGIN_FAILURE",
+        payload: err.response.data,
+      });
+    }
   };
   return (
     <div className="login">
