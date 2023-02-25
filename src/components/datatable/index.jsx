5d588a0c69e3e762aac75dfda3602f2599e65b07
Aravind104 <ontagodiaravind@gmail.com>
2023-02-25 14:00:02 +0530

    feat(components):add datatable component to display and modify room,user, hotel information

--- src/components/datatable/index.jsx
+++ src/components/datatable/index.jsx
@@ -1,17 +1,68 @@
 import { DataGrid } from "@mui/x-data-grid";
+import { Link, useLocation } from "react-router-dom";
 import "./datatable.scss";
+import { api } from "../../api";
+import { useEffect, useState } from "react";
+import useFetch from "../../hooks/useFetch";
+import axios from "axios";
 
-function DatatableComponent() {
+function DatatableComponent({ columns }) {
+  const location = useLocation();
+  const path = location.pathname.split("/")[1];
+  const [list, setList] = useState();
+  const { data, loading, error } = useFetch(`${api}/api/v1/${path}`);
+
+  useEffect(() => {
+    setList(data);
+  }, [data]);
+
+  const handleDelete = async (id) => {
+    try {
+      await axios.delete(`${api}/api/v1/${path}/${id}`);
+      setList(list.filter((item) => item._id !== id));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  const actionColumn = [
+    {
+      field: "action",
+      headerName: "Action",
+      width: 200,
+      renderCell: (params) => {
+        return (
+          <div>
+            <Link to="/users/test" style={{ textDecoration: "none" }}>
+              <div className="viewButton">View</div>
+            </Link>
+            <div
+              className="deleteButton"
+              onClick={() => handleDelete(params.row._id)}
+            >
+              Delete
+            </div>
+          </div>
+        );
+      },
+    },
+  ];
   return (
     <div className="datatable">
+      <div className="datatableTitle">
+        {path}{" "}
+        <Link to={`/${path}/new`} className="link">
+          Add New
+        </Link>
+      </div>
       <DataGrid
-        rows={rows}
-        columns={columns}
-        pageSize={5}
-        rowsPerPageOptions={[5]}
+        className="datagrid"
+        rows={list}
+        columns={columns.concat(actionColumn)}
+        pageSize={9}
+        rowsPerPageOptions={[9]}
         checkboxSelection
         disableSelectionOnClick
-        experimentalFeatures={{ newEditingApi: true }}
+        getRowId={(row) => row._id}
       />
     </div>
   );
